7f73bc28c5101ddbe4914321c6bcc37d32401670
Yarygina_IY <i_ya21&1700>
2021-03-25 17:24:08 +0300

    commit

---
+++ lib/task.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Task extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
